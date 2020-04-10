db761eac887075f93102ac2c6fd46075b296aba8
magohector <mago_hamp@hotmail.com>
2020-04-10 18:43:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='sklearnkernels',
     packages=['sklearnkernels'], 
-    version='0.1',
+    version='0.1.0',
     license='GPL', 
     description='A random test lib',
     author='Mora Hector',
